ccc15d85633cbe080d15add70309653942186a9d
ikhwanmatrazak <ikhwanmatrazak@gmail.com>
2026-05-10 14:05:12 +0800

    mufflux logo background remover
    mufflux logo background remover

--- frontend/components/ui/Navbar.tsx
+++ frontend/components/ui/Navbar.tsx
@@ -55,7 +55,7 @@ export default function SiteNavbar() {
             className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:mr-auto"
           >
             <Image
-              src="/mufflux.jpeg"
+              src="/mufflux.png"
               alt="Mufflux"
               width={160}
               height={54}
